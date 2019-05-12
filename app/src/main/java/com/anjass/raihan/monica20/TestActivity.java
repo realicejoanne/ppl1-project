929f09f5af68fd815c0ea42d1e4bfb361f279933
RaihanAk <raihandoank@ymail.com>
2019-05-12 15:12:23 +0700

    committee view dengan archiving

--- app/src/main/java/com/anjass/raihan/monica20/TestActivity.java
+++ app/src/main/java/com/anjass/raihan/monica20/TestActivity.java
@@ -1,37 +1,36 @@
 package com.anjass.raihan.monica20;
 
-import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.os.Handler;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.DatePicker;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class TestActivity extends AppCompatActivity {
 
-    AlertDialog.Builder dialog;
+    AlertDialog finishDialog, loadingFinishDialog;
+    AlertDialog.Builder archiveDialog, loadingDialog;
     LayoutInflater inflater;
     View dialogView;
 
-    private TextView result;
     private DatePicker datePicker;
     private TimePicker timePicker;
     private Button dialogBtn;
+    private TextView result, progressText, archivingStatusText,
+        askDialogYes_btn, askDialogNo_btn;
+    private ProgressBar progressBar;
+
+    private int currentProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,4 +78,89 @@ public class TestActivity extends AppCompatActivity {
         //result.setText("" +difference);
     }
 
+    public void finishTestDialog(View v){
+        // Setup the alert builder
+        archiveDialog = new AlertDialog.Builder(TestActivity.this);
+
+        inflater = getLayoutInflater();
+        dialogView = inflater.inflate(R.layout.finish_committee_archiving_ask, null);
+
+        archiveDialog.setView(dialogView);
+        archiveDialog.setCancelable(true);
+
+        finishDialog = archiveDialog.create();
+        finishDialog.show();
+
+
+        // Codes ============================
+
+        askDialogYes_btn = (TextView) dialogView.findViewById(R.id.askDialogYes_btn);
+        askDialogYes_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadingDialog();
+            }
+        });
+
+        askDialogNo_btn = (TextView) dialogView.findViewById(R.id.askDialogNo_btn);
+        askDialogNo_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishDialog.cancel();
+            }
+        });
+
+    }
+
+    private void loadingDialog(){
+        // Setup the alert builder
+        loadingDialog = new AlertDialog.Builder(TestActivity.this);
+
+        inflater = getLayoutInflater();
+        dialogView = inflater.inflate(R.layout.finish_committee_archiving_loading, null);
+
+        loadingDialog.setView(dialogView);
+        loadingDialog.setCancelable(true);
+
+        loadingFinishDialog = loadingDialog.create();
+        loadingFinishDialog.show();
+
+
+        // Codes Asyique =================
+
+        progressBar = (ProgressBar) dialogView.findViewById(R.id.progressBar);
+        progressText = (TextView) dialogView.findViewById(R.id.progressText);
+        archivingStatusText = (TextView) dialogView.findViewById(R.id.archivingStatusText);
+
+        currentProgress = 0;
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while(currentProgress <= 100){
+                        progressBar.setProgress(currentProgress);
+                        progressText.setText(currentProgress +"%");
+                        currentProgress += 1;
+                        Thread.sleep(30);
+                    }
+
+                    progressText.setText("Complete!");
+                    archivingStatusText.setText("Close this dialog");
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+
+        archivingStatusText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishDialog.cancel();
+                loadingFinishDialog.cancel();
+                finish();
+            }
+        });
+    }
 }
